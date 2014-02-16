1fb94fb1fd93ec485a37a7b78e971a8fd95d8962
shana <shana@derpymail.org>
2014-02-16 18:42:53 +0100

    Added some more exceptions

--- hearthy/exceptions.py
+++ hearthy/exceptions.py
@@ -3,3 +3,14 @@ class DecodeError(Exception):
 
 class CardNotFound(Exception):
     pass
+
+class EntityNotFound(Exception):
+    def __init__(self, eid):
+        super().__init__('Could not find entity with id={0}'.format(eid))
+
+class UnexpectedEof(Exception):
+    def __init__(self):
+        super().__init__('Encountered an unepxected end of file')
+
+class BufferFullException(Exception):
+    pass
