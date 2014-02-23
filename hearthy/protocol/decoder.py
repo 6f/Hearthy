ecfe6d8d605539b6c9122fd1b57b7022c050af52
shana <shana@derpymail.org>
2014-02-23 19:18:29 +0100

    Added encode_packet

--- hearthy/protocol/decoder.py
+++ hearthy/protocol/decoder.py
@@ -2,34 +2,47 @@
 Hearthstone Protocol Decoder.
 """
 
+import struct
 from hearthy.protocol import mtypes
 from hearthy.protocol.enums import PacketType
-from hearthy.exceptions import DecodeError
 
-_packet_type_handlers = {
-    PacketType.POWER_HISTORY: mtypes.PowerHistory,
-    PacketType.USER_UI: mtypes.UserUI,
-    PacketType.TURN_TIMER: mtypes.TurnTimer,
-    PacketType.START_GAME_STATE: mtypes.StartGameState,
-    PacketType.PRE_LOAD: mtypes.PreLoad,
-    PacketType.PRE_CAST: mtypes.PreCast,
-    PacketType.NOTIFICATION: mtypes.Notification,
-    PacketType.NACK_OPTION: mtypes.NAckOption,
-    PacketType.GIVE_UP: mtypes.GiveUp,
-    PacketType.GET_GAME_STATE: mtypes.GetGameState,
-    PacketType.GAME_SETUP: mtypes.GameSetup,
-    PacketType.GAME_CANCELLED: mtypes.GameCancelled,
-    PacketType.FINISH_GAME_STATE: mtypes.FinishGameState,
-    PacketType.ENTITY_CHOICE: mtypes.EntityChoice,
-    PacketType.DEBUG_MESSAGE: mtypes.DebugMessage,
-    PacketType.CLIENT_PACKET: mtypes.ClientPacket,
-    PacketType.CHOOSE_OPTION: mtypes.ChooseOption,
-    PacketType.CHOOSE_ENTITIES: mtypes.ChooseEntities,
-    PacketType.ALL_OPTIONS: mtypes.AllOptions,
-    PacketType.BEGIN_PLAYING: mtypes.BeginPlaying,
-    PacketType.AURORA_HANDSHAKE: mtypes.AuroraHandshake,
-    PacketType.GAME_STARTING: mtypes.GameStarting
-}
+_packet_type_map = [
+    (PacketType.POWER_HISTORY, mtypes.PowerHistory),
+    (PacketType.USER_UI, mtypes.UserUI),
+    (PacketType.TURN_TIMER, mtypes.TurnTimer),
+    (PacketType.START_GAME_STATE, mtypes.StartGameState),
+    (PacketType.PRE_LOAD, mtypes.PreLoad),
+    (PacketType.PRE_CAST, mtypes.PreCast),
+    (PacketType.NOTIFICATION, mtypes.Notification),
+    (PacketType.NACK_OPTION, mtypes.NAckOption),
+    (PacketType.GIVE_UP, mtypes.GiveUp),
+    (PacketType.GET_GAME_STATE, mtypes.GetGameState),
+    (PacketType.GAME_SETUP, mtypes.GameSetup),
+    (PacketType.GAME_CANCELLED, mtypes.GameCancelled),
+    (PacketType.FINISH_GAME_STATE, mtypes.FinishGameState),
+    (PacketType.ENTITY_CHOICE, mtypes.EntityChoice),
+    (PacketType.DEBUG_MESSAGE, mtypes.DebugMessage),
+    (PacketType.CLIENT_PACKET, mtypes.ClientPacket),
+    (PacketType.CHOOSE_OPTION, mtypes.ChooseOption),
+    (PacketType.CHOOSE_ENTITIES, mtypes.ChooseEntities),
+    (PacketType.ALL_OPTIONS, mtypes.AllOptions),
+    (PacketType.BEGIN_PLAYING, mtypes.BeginPlaying),
+    (PacketType.AURORA_HANDSHAKE, mtypes.AuroraHandshake),
+    (PacketType.GAME_STARTING, mtypes.GameStarting)
+]
+
+_packet_type_handlers = dict(_packet_type_map)
+_packet_type_id = dict((y,x) for x,y in _packet_type_map)
+
+def encode_packet(packet, buf, offset=0):
+    end = packet.encode_buf(buf, offset + 8)
+    packet_type = _packet_type_id.get(packet.__class__, None)
+    
+    if packet_type is None:
+        raise EncodeError('No packet type for class {0}'.format(packet.__class__))
+    
+    buf[offset:offset+8] = struct.pack('<II', packet_type, end - offset - 8)
+    return end
 
 def decode_packet(packet_type, buf):
     handler = _packet_type_handlers.get(packet_type, None)
